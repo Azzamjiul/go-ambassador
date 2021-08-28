3675dc5e96ff1b7c91e16004711d82bcb11847f1
Azzam Jihad Ulhaq <azzam.jihad@mekari.com>
2021-08-28 21:31:31 +0700

    Update profile and password

--- src/controllers/authController.go
+++ src/controllers/authController.go
@@ -93,11 +93,8 @@ func Login(c *fiber.Ctx) error {
 
 func User(c *fiber.Ctx) error {
 	id, _ := middlewares.GetUserId(c)
-
 	var user models.User
-
 	database.DB.Where("id = ?", id).First(&user)
-
 	return c.JSON(user)
 }
 
@@ -114,4 +111,51 @@ func Logout(c *fiber.Ctx) error {
 	return c.JSON(fiber.Map{
 		"message": "success",
 	})
+}
+
+func UpdateInfo(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	id, _ := middlewares.GetUserId(c)
+
+	user := models.User{
+		Id:        id,
+		FirstName: data["first_name"],
+		LastName:  data["last_name"],
+		Email:     data["email"],
+	}
+
+	database.DB.Model(&user).Updates(&user)
+
+	return c.JSON(user)
+}
+
+func UpdatePassword(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	if data["password"] != data["password_confirm"] {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "password do not match",
+		})
+	}
+
+	id, _ := middlewares.GetUserId(c)
+
+	user := models.User{
+		Id: id,
+	}
+
+	user.SetPassword(data["password"])
+	database.DB.Model(&user).Updates(&user)
+
+	return c.JSON(user)
 }
