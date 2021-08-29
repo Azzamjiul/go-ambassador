2aecf8c55e62bc60aa38b6b23fa0c22ada7dd50d
Azzam Jihad Ulhaq <azzam.jihad@mekari.com>
2021-08-29 10:38:57 +0700

    Aliases

--- src/controllers/authController.go
+++ src/controllers/authController.go
@@ -104,6 +104,11 @@ func User(c *fiber.Ctx) error {
 	id, _ := middlewares.GetUserId(c)
 	var user models.User
 	database.DB.Where("id = ?", id).First(&user)
+	if strings.Contains(c.Path(), "/api/ambassador") {
+		ambassador := models.Ambassador(user)
+		ambassador.CalculateRevenue(database.DB)
+		return c.JSON(ambassador)
+	}
 	return c.JSON(user)
 }
 
